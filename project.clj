88b062cf7565e1fc54d183d7b9d791e0b28cd5a5
Dell Adams <dradams@alumni.calpoly.edu>
2015-02-05 21:48:32 -0800

    Updated heroku domain name in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject mastery-timer "1.0.0-SNAPSHOT"
   :description "Mastery requires 10,000 hours of deliberate practice. When will your hour strike?"
-  :url "http://example.com/FIXME"
+  :url "https://immense-oasis-9808.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
