ceb04893df491796f144ff9418ebffebb0993efa
Dell Adams <dradams@alumni.calpoly.edu>
2015-02-05 22:08:09 -0800

    Properly invoked mastery-date

--- src/mastery_timer/web.clj
+++ src/mastery_timer/web.clj
@@ -22,7 +22,7 @@
   {:status 200
    :headers {"Content-Type" "text/plain"}
    :body (pr-str "At this rate, you will log your 10,000th hour on" 
-                 (tf/unparse date-formatter mastery-date my-start my-hours))})
+                 (tf/unparse date-formatter (mastery-date my-start my-hours)))})
 
 (defroutes app
   (GET "/" []
