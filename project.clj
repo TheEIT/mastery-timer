a57060ad12e119ef57767a3e3c036f8ea51a6ef1
Dell Adams <dradams@alumni.calpoly.edu>
2015-02-05 21:46:27 -0800

    Brought over resources directory from heroku getting-started app

--- project.clj
+++ project.clj
@@ -1,9 +1,17 @@
-(defproject mastery-timer "0.1.0-SNAPSHOT"
+(defproject mastery-timer "1.0.0-SNAPSHOT"
   :description "Mastery requires 10,000 hours of deliberate practice. When will your hour strike?"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
+  :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]
+                 [ring/ring-jetty-adapter "1.2.2"]
+                 [environ "0.5.0"]
                  [joda-time "2.6"]
                  [clj-time "0.9.0"]]
+  :min-lein-version "2.0.0"
+  :plugins [[environ/environ.lein "0.2.1"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "clojure-getting-started-standalone.jar"
+  :profiles {:production {:env {:production true}}})
   :main mastery-timer.core)
