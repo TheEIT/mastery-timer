9f6960abed1930824406f587b45103561efffc8b
Dell Adams <dradams@alumni.calpoly.edu>
2015-03-11 11:10:25 -0700

    Made EDN output test work. It also passes

--- test/mastery_timer/core_test.clj
+++ test/mastery_timer/core_test.clj
@@ -5,17 +5,18 @@
             [mastery-timer.core :refer :all]))
 
 (deftest date-computation-test
-  (testing "that the computed date is correct"
+  (testing "The computed date is to be correct"
     (is (= 10000 (mastery-date (t/today-at-midnight) 1)
                  (t/plus (t/today-at-midnight) (t/days 10000))))))
 
 (deftest edn-datastore-test
-  (testing "that the EDN datastore"
-    (testing "is receiving legible input" nil)
-    (testing "provides legible output"
-      (is (let [{:keys} (read-string (slurp "testdata.edn"))]
-            (and (= 17 (:hours testuserid))
-                 (= "January 25, 2015" 
-                    (tf/unparse date-formatter 
-                      (t/date-midnight 
-                        (:start-date testuserid))))))))))
+  (testing "The EDN datastore is to"
+    (testing "receive legible input"
+	  (is false))
+    (testing "provide legible output"
+      (let [{:keys [testuserid]} 
+	            (read-string (slurp "test/mastery_timer/testdata.edn"))]
+        (is (= 18 (:hours testuserid)))
+        (is (= "Sunday, January 25, 2015" 
+               (tf/unparse date-formatter 
+                 (apply t/date-midnight (:start-date testuserid)))))))))
